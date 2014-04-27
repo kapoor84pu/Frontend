66a1b76ed1e4597517414a109d0f9f1d051eb7c5
kapoor84pu <kapoor84pu@gmail.com>
2014-04-27 13:04:35 +0100

    AA

---
+++ bin/com/fdm/js/SaveBears.js
@@ -0,0 +1,41 @@
+function myfunction(){
+	var nextShowing = getNextShowing(movie1);
+	document.write(nextShowing);
+	//alert(nextShowing);
+}
+
+var movie1 = {
+	title: "Plan 9 from outer space",
+	genre: "RomCom",
+	rating: 5,
+	showtimes: ["3:00pm", "7:00pm", "11:00pm"]
+};
+
+var movie2 = {
+	title: "Forbidden Planet",
+	genre: "Scifi",
+	rating: 4,
+	showtimes: ["5:00pm", "9:00pm"]
+};
+
+function getNextShowing(movie){
+	var now = new Date().getTime();
+	
+	for(var i=0; i<movie.showtimes.length;i++){
+		var showtime = getTimeFromString(movie.showtimes[i]);
+		if((showtime-now)>0){
+			return "Next showing of " + movie.title + " is " + movie.showtimes[i];
+		}
+		return null;
+	}
+}
+
+function getTimeFromString(timeString){
+	var theTime = new Date();
+	var time = timeString.match(/(\d+)(?::(\d\d))?\s*(p?)/);
+	theTime.setHours(parseInt(time[1])+(time[3] ? 12 : 0));
+	theTime.setMinutes(parseInt(time[2]) || 0);
+	return theTime.getTime();
+}
+
+
